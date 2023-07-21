ecd008e41231a58ce3325d5d9aaa17fe589d9478
Justin <jmpark@email.wm.edu>
2023-07-21 14:33:02 -0400

    Set up routes for Navbar pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import Navbar from './components/Navbar';
+import Registration from './pages/Registration';
+import ClassInfo from './pages/ClassInfo';
+import Staff from './pages/Staff';
+import Home from './pages/Home';
 
 function App() {
-  return <Navbar/>
+  let component;
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home/>;
+      break;
+    case "/registration":
+      component = <Registration/>;
+      break;
+    case "/classInfo":
+      component = <ClassInfo/>;
+      break;
+    case "/staff":
+      component = <Staff/>;
+      break;
+  }
+  return (
+    <>
+      <Navbar/>
+      {component}
+    </>
+  )
+
 }
 
 export default App;
---
+++ src/pages/ClassInfo.js
@@ -0,0 +1,3 @@
+export default function ClassInfo() {
+    return <h1>Class Info</h1>
+}
