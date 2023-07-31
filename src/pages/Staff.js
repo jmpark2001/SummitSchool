c6e92d53ed02cb6ef07251ce4fde665e84161a41
Justin <jmpark@email.wm.edu>
2023-07-31 17:32:37 -0400

    Create Staff page template

--- src/pages/Staff.js
+++ src/pages/Staff.js
@@ -1,14 +1,55 @@
 export default function Staff() {
     return (
-        <div>
-            <ul>
-                <li>Principal: Rev. Dong C. Lee</li>
-                <li>Spiritual Advisor: Rev. Brendyn Huh</li>
-                <li>Director: Hyun-chul- Kim</li>
-                <li>Administrative Director: Christine Baek</li>
-                <li>Administrative Assisstants: Lou Ann Yang, Alexandra Garcia, Yeobeen Yun</li>
-                <li>Financial Director: Sung-kyung Yoo</li>
-            </ul>
+        <div className="staffPage">
+            <h1 className="aboutTitle">Meet the Staff</h1>
+            <div className="staffRow">
+                <div className="staffColumn">
+                    <p>Principal</p>
+                    <p>Rev. Dong C. Lee</p>
+                </div>
+                <div className="staffColumn">
+                    <p>Spiritual Advisor</p>
+                    <p>Rev. Brendyn Huh</p>
+                </div>
+            </div>
+            <div className="staffRow">
+                <div className="staffColumn">
+                    <p>Director</p>
+                    <p>Hyun-chul Kim</p>
+                </div>
+                <div className="staffColumn">
+                    <p>Administrative Director</p>
+                    <p>Christine Baek</p>
+                </div>
+            </div>
+            <div className="staffRow">
+                <div className="staffColumn">
+                    <p>Administrative Assistant</p>
+                    <p>Lou Ann Yang</p>
+                </div>
+                <div className="staffColumn">
+                    <p>Administrative Assistant</p>
+                    <p>Alexandra Garcia</p>
+                </div>
+                <div className="staffColumn">
+                    <p>Administrative Assistant</p>
+                    <p>Yeobeen Yun</p>
+                </div>
+            </div>
+            <div className="staffRow">
+                <div className="staffColumn">
+                    <p>Financial Director</p>
+                    <p>Sung-kyung Yoo</p>
+                </div>
+                <div className="staffColumn">
+                    <p>Website Developer</p>
+                    <p>Justin Park</p>
+                </div>
+                <div className="staffColumn">
+                    <p>Website Developer</p>
+                    <p>Joanne Lee</p>
+                </div>
+            </div>
         </div>
     )
 }
