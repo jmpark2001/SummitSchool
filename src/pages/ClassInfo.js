214966f814f000d3e87087121dd757d9d119b48d
Justin <jmpark@email.wm.edu>
2023-08-20 23:34:17 -0400

    Added banner for class schedule in home

--- src/pages/ClassInfo.js
+++ src/pages/ClassInfo.js
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom"
-
 export default function ClassInfo() {
     const classList = ["Bibilical Worldview", "Church History", "Old Testament", "New Testament", "Systematic Theology", "Korean", "Spanish", "Chinese", "Japanese", "Adult Korean", "Adult English", "Guitar", "Piano", "Flute",
      "Violin", "Drum", "SAT Math", "SAT English","Pre-K/K", "Basketball", "Stretching and Mobility"]
@@ -15,21 +13,6 @@ export default function ClassInfo() {
                 <p>Summit School classes will take place every Saturday from 9am-12pm. From 9-10 we will have worship and two classes will occur at 10-11 and 11-12. Students are required to take at least 1 biblical class and can take 1 class of their choice.</p>
                 <p>The fall semester begins on September 16th and ends on January 20th.</p>
                 <p>The fee for summit school will be $200 per semester and the payment information will be on the registration page.</p>
-                {/* <div className="classInfoClasses">
-                    <div className="classInfoDiv">
-                        <Link to={"/biblical"} className="classInfoTitles">Biblical Classes</Link>
-                    </div>
-                    <div className="classInfoDiv">
-                        <Link to={"/language"} className="classInfoTitles">Language Classes</Link>
-                    </div>
-                    <div className="classInfoDiv">
-                        <Link to={"/instrument"} className="classInfoTitles">Instrument Classes</Link>
-                    </div>
-                    <div className="classInfoDiv">
-                        <Link to={"/academic"} className="classInfoTitles">Academic Classes</Link>
-                    </div>
-                </div> */}
-                
                 <div className="classInfoClasses">
                     <div className="classInfoDiv">
                         <p className="classInfoDivTitle">Biblical</p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="homePage">
             <div className="homeDiv">
                 <div className="homeTitleDiv">
-                    <img src={omc} className="omcLogo"></img>
+                    <img src={omc} className="omcLogo" alt=""></img>
                     <div className="homeTitle">
                         <p className="summit">SUMMIT&nbsp;SCHOOL</p>
                         <p className="homeSubtitle">Platform, Watchtower, and Antenna for our future generations.</p>
@@ -22,7 +22,7 @@ export default function Home() {
             </div>
             <div className="fallSemester">
                 <p style={{margin: "2vh 0"}}>FALL SEMESTER</p>
-                <p style={{margin: "0 auto 2vh auto", width: "70%", fontWeight: "100", fontFamily: 'GlacialIndifferenceRegular, serif'}}>Registration for the fall semester starts on August 14th and will end on September 3rd.</p>
+                <p style={{margin: "0 auto 2vh auto", width: "70%", fontWeight: "100", fontFamily: 'GlacialIndifferenceRegular, serif'}}>Registration for the fall semester starts on Aug 14 and will end on Sep 3.</p>
                 <div style={{display: "flex", justifyContent: "center", marginBottom: "4vh"}}>
                     <Link to={"/registration"} style={{textDecoration: "none", display: "flex", width: "fit-content"}}>
                         <div className="registerNow">
@@ -36,40 +36,45 @@ export default function Home() {
             <div className="homeLinks">
                 <Link to={"/biblical"} className="homeLink">
                     <div className="biblicalLink">
-                        <img src={biblical}></img>
+                        <img src={biblical} alt=""></img>
                         <p style={{position:"absolute", top: "0%", right: "0%"}} className="homeLinkTitles">BIBLICAL</p>
                     </div>
                 </Link>
                 <Link to={"/language"} className="homeLink">
                     <div className="biblicalLink">
-                        <img src={language}></img>
+                        <img src={language} alt=""></img>
                         <p style={{position:"absolute", top: "0%", right: "0%"}} className="homeLinkTitles">LANGUAGE</p>
                     </div>
                 </Link>  
                 <Link to={"/instrument"} className="homeLink">
                     <div className="biblicalLink">
-                        <img src={instrument}></img>
+                        <img src={instrument} alt=""></img>
                         <p style={{position:"absolute", top: "0%", right: "0%"}} className="homeLinkTitles">INSTRUMENTS</p>
                     </div>
                 </Link>  
                 <Link to={"/academic"} className="homeLink">
                     <div className="biblicalLink">
-                        <img src={academic}></img>
+                        <img src={academic} alt=""></img>
                         <p style={{position:"absolute", top: "0%", right: "0%"}} className="homeLinkTitles">ACADEMIC</p>
                     </div>
                 </Link>  
                 <Link to={"/kindergarten"} className="homeLink">
                     <div className="biblicalLink">
-                        <img src={kindergarten}></img>
-                        <p style={{position:"absolute", top: "40%", right: "0%", lineHeight: "normal"}} className="homeLinkTitles">PRE-K/KINDERGARTEN</p>
+                        <img src={kindergarten} alt=""></img>
+                        <p style={{position:"absolute", top: "40%", right: "0%", lineHeight: "normal"}} className="homeLinkTitles">PRE-K/ KINDERGARTEN</p>
                     </div>
                 </Link>  
                 <Link to={"/pe"} className="homeLink">
                     <div className="biblicalLink">
-                        <img src={pe}></img>
+                        <img src={pe} alt=""></img>
                         <p style={{position:"absolute", top: "40%", right: "0%", lineHeight: "normal"}} className="homeLinkTitles">PHYSICAL EDUCATION</p>
                     </div>
                 </Link>  
+            </div>
+            <div style={{display: "relative", margin: "10vh 0"}}>
+                <div className="classScheduleBanner">
+                    <p>Classes are held every Saturday except Remnant Day (Last Saturday of every month) from 9/16 to 1/20</p>
+                </div>
             </div>
         </div>
     )
